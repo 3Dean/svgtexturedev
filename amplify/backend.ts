f8a962314ac8b72b6a9ff016a10f0285fa14bf60
Dean Yeager <dean@studio10b.com>
2025-03-27 16:42:25 -0400

    Commit number 2

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
+// amplify/backend.ts
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-import { data } from './data/resource';
+
+// No data import
 
 export const backend = defineBackend({
   auth,
-  data,
+  // No data here
 });
